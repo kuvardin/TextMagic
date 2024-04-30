060250373387b092211ce4702e5853c68583d6dd
Maxim Kuvardin <maxim@kuvard.in>
2024-04-30 13:25:12 +0500

    Bug fixed

---
+++ migrations/Version20240426123146.php
@@ -0,0 +1,82 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20240426123146 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('CREATE SEQUENCE answer_variant_id_seq INCREMENT BY 1 MINVALUE 1 START 1');
+        $this->addSql('CREATE SEQUENCE question_id_seq INCREMENT BY 1 MINVALUE 1 START 1');
+        $this->addSql('CREATE SEQUENCE test_id_seq INCREMENT BY 1 MINVALUE 1 START 1');
+        $this->addSql('CREATE SEQUENCE test_entity_id_seq INCREMENT BY 1 MINVALUE 1 START 1');
+        $this->addSql('CREATE SEQUENCE testtest_id_seq INCREMENT BY 1 MINVALUE 1 START 1');
+        $this->addSql('CREATE TABLE answer_variant (id INT NOT NULL, question_id INT NOT NULL, text VARCHAR(1023) NOT NULL, is_correct BOOLEAN NOT NULL, created_at TIMESTAMP(0) WITHOUT TIME ZONE NOT NULL, PRIMARY KEY(id))');
+        $this->addSql('CREATE INDEX IDX_B90370DC1E27F6BF ON answer_variant (question_id)');
+        $this->addSql('COMMENT ON COLUMN answer_variant.created_at IS \'(DC2Type:datetime_immutable)\'');
+        $this->addSql('CREATE TABLE question (id INT NOT NULL, test_id INT NOT NULL, text VARCHAR(1023) NOT NULL, created_at TIMESTAMP(0) WITHOUT TIME ZONE NOT NULL, PRIMARY KEY(id))');
+        $this->addSql('CREATE INDEX IDX_B6F7494E1E5D0459 ON question (test_id)');
+        $this->addSql('COMMENT ON COLUMN question.created_at IS \'(DC2Type:datetime_immutable)\'');
+        $this->addSql('CREATE TABLE test (id INT NOT NULL, name VARCHAR(255) NOT NULL, description VARCHAR(1023) NOT NULL, created_at TIMESTAMP(0) WITHOUT TIME ZONE NOT NULL, PRIMARY KEY(id))');
+        $this->addSql('COMMENT ON COLUMN test.created_at IS \'(DC2Type:datetime_immutable)\'');
+        $this->addSql('CREATE TABLE test_entity (id INT NOT NULL, test_id INT NOT NULL, PRIMARY KEY(id))');
+        $this->addSql('CREATE TABLE testing (id UUID NOT NULL, test_id INT NOT NULL, score INT DEFAULT NULL, created_at TIMESTAMP(0) WITHOUT TIME ZONE NOT NULL, PRIMARY KEY(id))');
+        $this->addSql('CREATE INDEX IDX_E8F35A061E5D0459 ON testing (test_id)');
+        $this->addSql('COMMENT ON COLUMN testing.id IS \'(DC2Type:uuid)\'');
+        $this->addSql('COMMENT ON COLUMN testing.created_at IS \'(DC2Type:datetime_immutable)\'');
+        $this->addSql('CREATE TABLE testtest (id INT NOT NULL, PRIMARY KEY(id))');
+        $this->addSql('CREATE TABLE messenger_messages (id BIGSERIAL NOT NULL, body TEXT NOT NULL, headers TEXT NOT NULL, queue_name VARCHAR(190) NOT NULL, created_at TIMESTAMP(0) WITHOUT TIME ZONE NOT NULL, available_at TIMESTAMP(0) WITHOUT TIME ZONE NOT NULL, delivered_at TIMESTAMP(0) WITHOUT TIME ZONE DEFAULT NULL, PRIMARY KEY(id))');
+        $this->addSql('CREATE INDEX IDX_75EA56E0FB7336F0 ON messenger_messages (queue_name)');
+        $this->addSql('CREATE INDEX IDX_75EA56E0E3BD61CE ON messenger_messages (available_at)');
+        $this->addSql('CREATE INDEX IDX_75EA56E016BA31DB ON messenger_messages (delivered_at)');
+        $this->addSql('COMMENT ON COLUMN messenger_messages.created_at IS \'(DC2Type:datetime_immutable)\'');
+        $this->addSql('COMMENT ON COLUMN messenger_messages.available_at IS \'(DC2Type:datetime_immutable)\'');
+        $this->addSql('COMMENT ON COLUMN messenger_messages.delivered_at IS \'(DC2Type:datetime_immutable)\'');
+        $this->addSql('CREATE OR REPLACE FUNCTION notify_messenger_messages() RETURNS TRIGGER AS $$
+            BEGIN
+                PERFORM pg_notify(\'messenger_messages\', NEW.queue_name::text);
+                RETURN NEW;
+            END;
+        $$ LANGUAGE plpgsql;');
+        $this->addSql('DROP TRIGGER IF EXISTS notify_trigger ON messenger_messages;');
+        $this->addSql('CREATE TRIGGER notify_trigger AFTER INSERT OR UPDATE ON messenger_messages FOR EACH ROW EXECUTE PROCEDURE notify_messenger_messages();');
+        $this->addSql('ALTER TABLE answer_variant ADD CONSTRAINT FK_B90370DC1E27F6BF FOREIGN KEY (question_id) REFERENCES question (id) NOT DEFERRABLE INITIALLY IMMEDIATE');
+        $this->addSql('ALTER TABLE question ADD CONSTRAINT FK_B6F7494E1E5D0459 FOREIGN KEY (test_id) REFERENCES test (id) NOT DEFERRABLE INITIALLY IMMEDIATE');
+        $this->addSql('ALTER TABLE testing ADD CONSTRAINT FK_E8F35A061E5D0459 FOREIGN KEY (test_id) REFERENCES test (id) NOT DEFERRABLE INITIALLY IMMEDIATE');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('CREATE SCHEMA public');
+        $this->addSql('DROP SEQUENCE answer_variant_id_seq CASCADE');
+        $this->addSql('DROP SEQUENCE question_id_seq CASCADE');
+        $this->addSql('DROP SEQUENCE test_id_seq CASCADE');
+        $this->addSql('DROP SEQUENCE test_entity_id_seq CASCADE');
+        $this->addSql('DROP SEQUENCE testtest_id_seq CASCADE');
+        $this->addSql('ALTER TABLE answer_variant DROP CONSTRAINT FK_B90370DC1E27F6BF');
+        $this->addSql('ALTER TABLE question DROP CONSTRAINT FK_B6F7494E1E5D0459');
+        $this->addSql('ALTER TABLE testing DROP CONSTRAINT FK_E8F35A061E5D0459');
+        $this->addSql('DROP TABLE answer_variant');
+        $this->addSql('DROP TABLE question');
+        $this->addSql('DROP TABLE test');
+        $this->addSql('DROP TABLE test_entity');
+        $this->addSql('DROP TABLE testing');
+        $this->addSql('DROP TABLE testtest');
+        $this->addSql('DROP TABLE messenger_messages');
+    }
+}
